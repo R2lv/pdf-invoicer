f52ec095d5c91fc7db4e3104313c3cec00b7d786
George Tabatadze <george.tabatadze@fkt42.de>
2024-04-06 02:59:50 +0400

    Fixes

--- src/Invoice.php
+++ src/Invoice.php
@@ -470,7 +470,7 @@ class Invoice extends \FPDF_rotation  {
 					
 				}
 				$this->Cell($this->columnSpacing,$cHeight,'',0,0,'L',0);
-				$this->Cell($width_other,$cHeight,iconv('UTF-8', 'windows-1252', $this->currency.' '.number_format(floatval($item['price']),2,$this->referenceformat[0],$this->referenceformat[1])),0,0,'C',1);
+				$this->Cell($width_other,$cHeight,iconv('UTF-8', 'windows-1252', $this->currency.' '.number_format(floatval(preg_replace('/[^0-9. ]/', '', $item['price'])),2,$this->referenceformat[0],$this->referenceformat[1])),0,0,'C',1);
 				if(isset($this->discountField)) 
 				{
 					$this->Cell($this->columnSpacing,$cHeight,'',0,0,'L',0);
@@ -484,7 +484,7 @@ class Invoice extends \FPDF_rotation  {
 					}
 				}
 				$this->Cell($this->columnSpacing,$cHeight,'',0,0,'L',0);
-				$this->Cell($width_other,$cHeight,iconv('UTF-8', 'windows-1252', $this->currency.' '.number_format(floatval($item['total']),2,$this->referenceformat[0],$this->referenceformat[1])),0,0,'C',1);
+				$this->Cell($width_other,$cHeight,iconv('UTF-8', 'windows-1252', $this->currency.' '.number_format(floatval(preg_replace('/[^0-9. ]/', '', $item['total'])),2,$this->referenceformat[0],$this->referenceformat[1])),0,0,'C',1);
 				$this->Ln();
 				$this->Ln($this->columnSpacing);
 			}
